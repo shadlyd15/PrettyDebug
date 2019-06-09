dd63b0f8ef86d1e8b7ac80de5e46bd2775159a03
Shadly Salahuddin <shadlyd15@gmail.com>
2019-06-09 20:34:32 +0600

    Update PrettyDebug.h

--- src/PrettyDebug.h
+++ src/PrettyDebug.h
@@ -48,23 +48,25 @@
 #endif
 
 #if !defined(SUPPORT_COLOR_TEXT)
-    #define COLOR_BLACK              
-    #define COLOR_RED               
-    #define COLOR_GREEN             
-    #define COLOR_YELLOW            
-    #define COLOR_BLUE              
-    #define COLOR_MAGENTA           
-    #define COLOR_CYAN              
-    #define COLOR_WHITE             
+    #define COLOR_BLACK
+    #define COLOR_RED
+    #define COLOR_GREEN
+    #define COLOR_YELLOW
+    #define COLOR_BLUE
+    #define COLOR_MAGENTA
+    #define COLOR_CYAN
+    #define COLOR_WHITE
+    #define COLOR_RESET
 #else
     #define COLOR_BLACK             "\x1B[30m"
     #define COLOR_RED               "\x1B[31m"
     #define COLOR_GREEN             "\x1B[32m"
     #define COLOR_YELLOW            "\x1B[33m"
-    #define COLOR_BLUE              "\x1b[34m"
-    #define COLOR_MAGENTA           "\x1b[35m"
+    #define COLOR_BLUE              "\x1B[34m"
+    #define COLOR_MAGENTA           "\x1B[35m"
     #define COLOR_CYAN              "\x1B[36m"
     #define COLOR_WHITE             "\x1B[37m"
+    #define COLOR_RESET             "\x1B[00m"
 #endif  /* SUPPORT_COLOR_TEXT  */
 
 
